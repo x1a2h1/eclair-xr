19de8c11fe5d910ec6c6b25e232daffbc0503626
夏至 <tanchjim@aol.com>
2024-03-20 20:52:41 +0800

    导航栏

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -20,14 +20,14 @@ const method = {
   <el-menu :default-active="state.activeIndex" class="w-full" mode="horizontal" :ellipsis="false"
     @select="method.handleSelect()">
     <el-menu-item>
-      <RouterLink class="flex" to="/">
+      <RouterLink class="flex pl-4%" to="/">
         <img class="w-45 h-auto" src="https://inis.cc/assets/imgs/logo-white.png" alt="logo" />
       </RouterLink>
 
     </el-menu-item>
     <div class="flex-grow" />
     <el-menu-item index="1">Processing Center</el-menu-item>
-    <div class=" flex items-center justify-center">
+    <div class=" flex items-center justify-center pr-4%">
       <el-switch v-model="state.isDark">
         <template #active-action>亮</template>
         <template #inactive-action>暗</template>
